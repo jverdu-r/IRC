f876122e521fe0c266a54ee1f3e8801415aebea5
ManuArdeiro <ardeiro@gmail.com>
2025-03-28 12:28:28 +0100

    mas comentarios

--- src/socket_manager.cpp
+++ src/socket_manager.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   socket_manager.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jverdu-r <jverdu-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Ardeiro <Ardeiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 16:01:11 by jverdu-r          #+#    #+#             */
-/*   Updated: 2025/03/21 20:24:06 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2025/03/28 11:59:43 by Ardeiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@
 #include <cerrno> // Añadido para errno
 
 SocketManager::SocketManager(int port, const std::string& password)
-    : server_fd(socket(AF_INET, SOCK_STREAM, 0)),
-      epoll_fd(epoll_create1(0)),
-      server_password(password),
+    : server_password(password),
       client_addresses(), // Inicializar client_addresses
       nicknames(), // Inicializar nicknames
       authenticated_clients(), // Inicializar authenticated_clients
@@ -37,7 +35,8 @@ SocketManager::SocketManager(int port, const std::string& password)
         std::cerr << "Error al crear el socket del servidor." << std::endl;
         exit(1);
     }
-
+    // Activamos la opción SO_REUSEADDR para permitir reutilizar el puerto inmediatamente después de cerrar el servidor, evitando errores como "Address already in use".
+    // Es útil especialmente durante el desarrollo o reinicios frecuentes del servidor, ya que la espero puede ser de 1-4 minutos.
     int reuse = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1)
     {
@@ -45,14 +44,14 @@ SocketManager::SocketManager(int port, const std::string& password)
         exit(1);
     }
 
-    // 2. Configurar la direccion del servidor
+    // 2. Configurar la direccion del servidor. Se prepara una estructura para que el socket escuche en cualquier IP (0.0.0.0) y el puerto indicado.
     sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
+    memset(&server_address, 0, sizeof(server_address)); //Esto pone todos los bytes de la estructura server_address a cero.
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(port);
 
-    // 3. Asociar el socket a la direccion del servidor
+    // 3. Asociar el socket a la direccion del servidor; une el socket a la IP y puerto configurados.
     if (bind(server_fd, (sockaddr*)&server_address, sizeof(server_address)) == -1)
     {
         std::cerr << "Error al asociar el socket a la direccion." << std::endl;
@@ -95,37 +94,46 @@ SocketManager::~SocketManager()
 
 void SocketManager::run()
 {
-    epoll_event events[MAX_EVENTS];
+    epoll_event events[MAX_EVENTS]; //Reservamos un array donde epoll depositará los eventos que detecte.
 
-    while (true) {
-        int num_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    while (true)
+    {
+        // Gracias a (bind + listen) se que entran las solo las conexiones dirigidas a este programa.
+        int num_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1); // -1 indica que epoll_wait es bloqueante, espera indefinidamente
         if (num_events == -1)
         {
             std::cerr << "Error en epoll_wait: " << strerror(errno) << std::endl;
             break;
+            // Solo si epoll_wait devuelve -1 (por un error) se termina el bucle.
         }
 
         for (int i = 0; i < num_events; ++i)
         {
+            // events[i].events  indica qué tipo de evento ocurrió en ese descriptor, EPOLLIN es para comprobar
+            // si el evento incluye "datos disponibles para leer". El & es un AND bit a bit. Si el resultado es 
+            // distinto de cero, significa que el evento EPOLLIN ocurrió.
             if (events[i].events & EPOLLIN)
             {
+                // Si es el socket del servidor, es decir, si es server_fd, significa un cliente intenta conectarse.
                 if (events[i].data.fd == server_fd)
                 {
                     acceptConnection();
                 }
+                // Si no, si el evento viene de un socket de cliente, se delega el procesamiento al manejador de eventos.
                 else
                 {
                     event_handler.handleClientEvent(events[i].data.fd);
                 }
             }
+            // Si no, si el evento es una desconexión (EPOLLHUP) o un error (EPOLLERR):
             else if (events[i].events & (EPOLLHUP | EPOLLERR))
             {
-                // Cliente desconectado o error
                 //std::cout << "Cliente " << events[i].data.fd << " desconectado o error." << std::endl;
-                epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
-                close(events[i].data.fd);
-                client_addresses.erase(events[i].data.fd);
-                partial_messages.erase(events[i].data.fd);
+                epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL); // Se elimina el cliente del epoll.
+                close(events[i].data.fd); // Se cierra el socket del cliente.
+                client_addresses.erase(events[i].data.fd); // Se eliminan sus datos de los mapas de IPs.
+                partial_messages.erase(events[i].data.fd); // Se eliminan sus mensajes parciales.
+                // El cliente ha desaparecido: ¡hay que dejar todo limpio!
             }
         }
     }
